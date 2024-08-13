57091d11dbfe76552b1393438ada11db2b1e3b47
ra00d <84684356+ra00d@users.noreply.github.com>
2024-08-13 06:07:47 +0300

    add front-end build

--- apps/front-end/src/lib/api/index.ts
+++ apps/front-end/src/lib/api/index.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 const apiClient = axios.create({
-	baseURL:
-		process.env.FARM_BASE_URL ||
-		"https://task-management-wf4j.onrender.com/api",
+	baseURL: "https://task-management-wf4j.onrender.com/api",
 	withXSRFToken: true,
 	withCredentials: true,
 	xsrfCookieName: "XSRF-TOKEN",
