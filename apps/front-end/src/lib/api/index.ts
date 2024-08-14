bbf22f0b67c5a78b52c6fcb309f0ce7c081a2f2c
ra00d <84684356+ra00d@users.noreply.github.com>
2024-08-14 03:11:04 +0300

    fix base url path

--- apps/front-end/src/lib/api/index.ts
+++ apps/front-end/src/lib/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiClient = axios.create({
 	// baseURL: FARM_BASE_URL || "127.0.0.1:3000/api",
-	baseURL: "api",
+	baseURL: "/api",
 	withXSRFToken: true,
 	withCredentials: true,
 	xsrfCookieName: "XSRF-TOKEN",
